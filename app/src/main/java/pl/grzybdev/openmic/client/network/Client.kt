d18761f1d47f866b1d5497383be4dc3249e65640
Marek Grzyb <grzybdev@gmail.com>
2022-05-28 21:06:14 +0200

    Handle received message from server

--- app/src/main/java/pl/grzybdev/openmic/client/network/Client.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/network/Client.kt
@@ -1,10 +1,20 @@
 package pl.grzybdev.openmic.client.network
 
 import android.util.Log
+import kotlinx.serialization.builtins.ListSerializer
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonObject
 import okhttp3.Response
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
-import pl.grzybdev.openmic.client.network.messages.client.Message
+import pl.grzybdev.openmic.client.network.messages.client.ClientMessage
+import pl.grzybdev.openmic.client.network.messages.server.MessageType
+import pl.grzybdev.openmic.client.network.messages.server.ServerMessage
+import pl.grzybdev.openmic.client.network.messages.server.packets.BasePacket
+import pl.grzybdev.openmic.client.network.messages.server.packets.ErrorPacket
+import pl.grzybdev.openmic.client.network.messages.server.packets.ServerPacket
 
 class Client : WebSocketListener() {
 
@@ -13,11 +23,20 @@ class Client : WebSocketListener() {
     override fun onOpen(webSocket: WebSocket, response: Response) {
         isListening = true
 
-        webSocket.send(Command.Get(Message.SYSTEM_HELLO))
+        webSocket.send(Handler.GetPacket(ClientMessage.SYSTEM_HELLO))
     }
 
     override fun onMessage(webSocket: WebSocket, text: String) {
-        Log.d(javaClass.name, "onMessage")
+        val json = Json { ignoreUnknownKeys = true }
+        val pBase: ServerPacket = json.decodeFromString(Handler.Companion.PacketSerializer, text)
+
+        if (pBase is BasePacket) {
+            val mType: ServerMessage = MessageType.fromString(pBase.type)
+            Handler.HandlePacket(webSocket, mType, text)
+        } else {
+            Log.e(javaClass.name, "Received error packet, disconnecting...")
+            webSocket.close(1006, "Received error packet")
+        }
     }
 
     override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
