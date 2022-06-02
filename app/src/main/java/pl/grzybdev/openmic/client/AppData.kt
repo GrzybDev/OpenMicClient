6cc868609c876c4122dc22c8ebacd08fde0c5e59
Marek Grzyb <grzybdev@gmail.com>
2022-06-02 16:33:58 +0200

    Added appPreferences and mainActivity to AppData object

--- app/src/main/java/pl/grzybdev/openmic/client/AppData.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/AppData.kt
@@ -1,6 +1,12 @@
 package pl.grzybdev.openmic.client
 
+import android.content.SharedPreferences
+import pl.grzybdev.openmic.client.activities.MainActivity
+
 object AppData {
+
+    var appPreferences: SharedPreferences? = null
+    var mainActivity: MainActivity? = null
 
     var deviceID: String = ""
 
