60b506eec5557f76c4ce55b48acc56341021f437
Marek Grzyb <grzybdev@gmail.com>
2022-06-03 21:02:03 +0200

    Improved handling input in auth dialog

--- app/src/main/java/pl/grzybdev/openmic/client/network/messages/server/System.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/network/messages/server/System.kt
@@ -1,7 +1,10 @@
 package pl.grzybdev.openmic.client.network.messages.server
 
-import android.app.AlertDialog
+import android. app.AlertDialog
+import android.text.Editable
 import android.text.InputType
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.EditText
 import kotlinx.serialization.Serializable
@@ -13,6 +16,7 @@ import pl.grzybdev.openmic.client.AppData
 import pl.grzybdev.openmic.client.R
 import pl.grzybdev.openmic.client.network.messages.ExitCode
 import pl.grzybdev.openmic.client.network.messages.Message
+import pl.grzybdev.openmic.client.network.messages.client.AuthCodeVerify
 import pl.grzybdev.openmic.client.network.messages.client.ClientPacket
 import pl.grzybdev.openmic.client.network.messages.client.SystemGoodbye as ClientSystemGoodbye
 
@@ -87,10 +91,15 @@ class SystemPacket {
                     // Set up the buttons
                     builder.setPositiveButton(AppData.mainActivity?.getString(R.string.AuthDialog_Button_OK)) { _, _ ->
                         run {
+                            val authCode = Integer.parseInt(input.text.toString())
+
                             Log.d(
                                 SystemPacket::class.java.name,
-                                "Entered code: " + input.text.toString()
+                                "Entered code: $authCode"
                             )
+
+                            val authPacket: ClientPacket = AuthCodeVerify(authCode)
+                            socket.send(Json.encodeToString(authPacket))
                         }
                     }
 
@@ -105,8 +114,42 @@ class SystemPacket {
                         }
                     }
 
-                    AppData.currentDialog = builder.create()
-                    AppData.currentDialog?.show()
+                    if (AppData.currentDialog?.isShowing == true)
+                        AppData.currentDialog?.dismiss()
+
+                    val dialog = builder.create()
+
+                    input.addTextChangedListener(object : TextWatcher {
+                        override fun beforeTextChanged(
+                            s: CharSequence?,
+                            start: Int,
+                            count: Int,
+                            after: Int
+                        ) {
+
+                        }
+
+                        override fun onTextChanged(
+                            s: CharSequence?,
+                            start: Int,
+                            before: Int,
+                            count: Int
+                        ) {}
+
+                        override fun afterTextChanged(s: Editable?) {
+                            // Disable verify button if input is empty
+                            dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = !TextUtils.isEmpty(s)
+                        }
+
+                    })
+
+                    dialog.show()
+
+                    // Disable verify button (initially)
+                    dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false
+
+                    // Save reference for dialog
+                    AppData.currentDialog = dialog
                 }
             }
         }
