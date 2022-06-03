cabdde01682084b3c862821e8a533bf31bd7692f
Marek Grzyb <grzybdev@gmail.com>
2022-06-03 21:01:08 +0200

    Updated ExitCodes

--- app/src/main/java/pl/grzybdev/openmic/client/network/messages/ExitCode.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/network/messages/ExitCode.kt
@@ -2,5 +2,7 @@ package pl.grzybdev.openmic.client.network.messages
 
 enum class ExitCode(val code: Int) {
     NORMAL(0),
-    CANCELED_AUTH_CODE_DIALOG(1)
+    VERSION_MISMATCH(1),
+    CANCELED_AUTH_CODE_DIALOG(2),
+    AUTH_CODE_INVALID(3)
 }
