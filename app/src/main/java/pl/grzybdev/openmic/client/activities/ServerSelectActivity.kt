17beed12d41860894aa75e57b4442a0bebf8816e
Marek Grzyb <grzybdev@gmail.com>
2022-06-14 22:48:32 +0200

    Created ServerSelectActivity

---
+++ app/src/main/java/pl/grzybdev/openmic/client/activities/ServerSelectActivity.kt
@@ -0,0 +1,12 @@
+package pl.grzybdev.openmic.client.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import pl.grzybdev.openmic.client.R
+
+class ServerSelectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_server_select)
+    }
+}
