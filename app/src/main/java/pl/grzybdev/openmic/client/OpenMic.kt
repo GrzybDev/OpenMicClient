91056b5e62db3824c14745026e831448f7294351
Marek Grzyb <grzybdev@gmail.com>
2022-06-16 17:06:57 +0200

    Finished Wi-Fi logic

--- app/src/main/java/pl/grzybdev/openmic/client/OpenMic.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/OpenMic.kt
@@ -5,8 +5,11 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.content.SharedPreferences
 import android.net.ConnectivityManager
+import android.net.wifi.WifiManager
+import android.os.PowerManager
 import android.util.Base64
 import android.util.Log
+import androidx.core.content.ContextCompat.getSystemService
 import com.gazman.signals.Signals
 import okhttp3.OkHttpClient
 import okhttp3.Request
@@ -25,6 +28,7 @@ import pl.grzybdev.openmic.client.receivers.WifiStateReceiver
 import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
+import java.net.SocketTimeoutException
 import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.concurrent.thread
@@ -32,15 +36,23 @@ import kotlin.concurrent.thread
 
 class OpenMic(context: Context) {
 
-    private lateinit var client: Client
+    private var client: Client? = null
     private lateinit var webSocket: WebSocket
 
     private val connectSignal = Signals.signal(IConnector::class)
-    private var broadcastThread: Thread? = null
 
     private val deviceID: String
 
+    private var autoConnectWifi = true
+    private var broadcastThread: Thread? = null
+
+    private var usbReceiver: USBStateReceiver? = null
+    private var wifiReceiver: WifiStateReceiver? = null
+
+    private var lastConnState: Boolean = false
+
     init {
+        App.context = this
         val deviceIDKey = context.getString(R.string.PREFERENCE_APP_DEVICE_ID)
 
         if (!App.appPreferences?.contains(deviceIDKey)!!) {
@@ -61,16 +73,67 @@ class OpenMic(context: Context) {
         // Init dialogs
         AuthDialog()
 
+        connectSignal.addListener { connector, event -> run {
+            when (connector) {
+                Connector.USB -> run {
+                    if (event == ConnectorEvent.DISABLED) {
+                        Log.d(javaClass.name, "USB functionality disabled, disconnected from PC")
+                    }
+
+                    if (event == ConnectorEvent.CONNECTING) {
+                        Log.d(javaClass.name, "Trying to connect to PC via USB...")
+                        connectTo(Connector.USB, "localhost")
+                    }
+                }
+
+                Connector.WiFi -> run {
+                    when (event) {
+                        ConnectorEvent.DISABLED -> run {
+                            autoConnectWifi = false
+                            broadcastThread?.interrupt()
+                        }
+
+                        ConnectorEvent.CONNECTING, ConnectorEvent.NEED_MANUAL_LAUNCH -> run {
+                            autoConnectWifi = false
+                        }
+
+                        ConnectorEvent.CONNECTED_OR_READY -> run {
+                            if (!lastConnState && client?.isConnected == true) {
+                                Log.d(javaClass.name, "Connected, interrupting broadcast thread...")
+                                autoConnectWifi = false
+                                broadcastThread?.interrupt()
+                            } else if (lastConnState && client?.isConnected == false) {
+                                Log.d(javaClass.name, "Not connected, initializing broadcast thread...")
+                                autoConnectWifi = true
+                                initWiFi()
+                            }
+
+                            lastConnState = client?.isConnected == true
+                            AppData.foundServers.clear()
+                        }
+                    }
+                }
+
+                Connector.Bluetooth -> run {
+                    TODO()
+                }
+            }
+        }}
+
         restartClient()
+
+        val wm = App.mainActivity?.applicationContext?.getSystemService(Context.WIFI_SERVICE) as WifiManager?
+        wm?.createWifiLock(WifiManager.WIFI_MODE_FULL_HIGH_PERF, "OpenMic:WifiLock")
     }
 
     object App {
         var appPreferences: SharedPreferences? = null
         var mainActivity: MainActivity? = null
+        var context: OpenMic? = null
     }
 
     private fun restartClient() {
-        if (this::webSocket.isInitialized && client.isConnected)
+        if (this::webSocket.isInitialized && client?.isConnected == true)
             webSocket.close(1012, App.mainActivity?.getString(R.string.WebSocket_Restart))
 
         AppData.communicationPort = App.appPreferences?.getInt(
@@ -78,17 +141,22 @@ class OpenMic(context: Context) {
             10000
         )!!
 
-        initUSB()
+        initReceivers()
+
         initWiFi()
     }
 
-    private fun connectTo(connector: Connector, address: String)
+    fun connectTo(connector: Connector, address: String)
     {
         if (AppData.connectLock) {
             return
         }
 
         AppData.connectLock = true
+
+        if (connector != Connector.USB)
+            connectSignal.dispatcher.onEvent(connector, ConnectorEvent.CONNECTING)
+
         Log.d(javaClass.name, "Trying to connect to $address...")
 
         client = Client(connector)
@@ -102,56 +170,65 @@ class OpenMic(context: Context) {
             .url("ws://$address:${AppData.communicationPort}")
             .build()
 
-        webSocket = httpClient.newWebSocket(webRequest, client)
+        webSocket = httpClient.newWebSocket(webRequest, client!!)
         httpClient.dispatcher.executorService.shutdown()
     }
 
-    private fun initUSB()
+    private fun initReceivers()
     {
-        connectSignal.addListener { connector, event -> run {
-            if (connector == Connector.USB) {
-                when (event) {
-                    ConnectorEvent.DISABLED -> run {
-                        Log.d(javaClass.name, "Disconnected from PC")
-                    }
+        if (usbReceiver != null) {
+            App.mainActivity?.unregisterReceiver(usbReceiver)
+            usbReceiver = null
+        }
 
-                    ConnectorEvent.CONNECTING -> run {
-                        Log.d(javaClass.name, "Connected to PC")
-                        connectTo(Connector.USB, "localhost")
-                    }
+        if (wifiReceiver != null) {
+            App.mainActivity?.unregisterReceiver(wifiReceiver)
+            wifiReceiver = null
+        }
 
-                    else -> {}
-                }
-            }
-        }}
+        usbReceiver = USBStateReceiver()
+        wifiReceiver = WifiStateReceiver()
 
-        App.mainActivity?.registerReceiver(USBStateReceiver(), IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+        App.mainActivity?.registerReceiver(usbReceiver, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
+
+        @Suppress("DEPRECATION")
+        App.mainActivity?.registerReceiver(wifiReceiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
     }
 
     private fun initWiFi()
     {
         broadcastThread?.interrupt()
-
-        val socket = DatagramSocket(AppData.communicationPort, InetAddress.getByName("0.0.0.0"))
-        socket.broadcast = true
-
-        val buf = ByteArray(1024)
-        val packet = DatagramPacket(buf, buf.size)
+        broadcastThread?.join()
 
         broadcastThread = thread(start = true) {
+            val broadcastSocket = DatagramSocket(AppData.communicationPort, InetAddress.getByName("0.0.0.0"))
+            broadcastSocket.broadcast = true
+            broadcastSocket.soTimeout = 1000
+
+            val buf = ByteArray(1024)
+            val packet = DatagramPacket(buf, buf.size)
+
             while (!Thread.interrupted()) {
                 Log.d(javaClass.name, "Waiting for broadcast on port ${AppData.communicationPort}...")
-                socket.receive(packet)
-                handleBroadcast(packet.data, packet.address.hostAddress!!.toString(), packet.port.toShort())
-            }
-        }
 
-        @Suppress("DEPRECATION")
-        App.mainActivity?.registerReceiver(WifiStateReceiver(), IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+                try {
+                    broadcastSocket.receive(packet)
+                } catch (e: SocketTimeoutException) {
+                    continue
+                }
+
+                handleBroadcast(packet.data, packet.address.hostAddress!!.toString())
+            }
+
+            broadcastSocket.close()
+        }
     }
 
-    private fun handleBroadcast(encodedData: ByteArray, senderIP: String, serverPort: Short)
+    private fun handleBroadcast(encodedData: ByteArray, senderIP: String)
     {
+        if (broadcastThread?.isInterrupted == true)
+            return
+
         Log.d(javaClass.name, "Received broadcast, analyzing it...")
         val decodedData = String(Base64.decode(encodedData, Base64.DEFAULT))
         val data = decodedData.split(";")
@@ -162,19 +239,52 @@ class OpenMic(context: Context) {
             return
         }
 
-        val entry = ServerEntry(data[3], senderIP, serverPort, getServerCompatibility(data[0], data[1]), getServerOS(data[2]), data[4])
+        val entry = ServerEntry(data[3], senderIP, getServerCompatibility(data[0], data[1]), getServerOS(data[2]), data[4])
+
+        AppData.foundServersTimestamps[entry.serverID] = Date().time
 
         if (!AppData.foundServers.contains(entry.serverID)) {
             // First time we received broadcast from this server, just add it to foundServers list
+            Log.d(javaClass.name, "Received first broadcast packet from ${entry.serverID}, starting heartbeat thread...")
             AppData.foundServers[entry.serverID] = entry
+
+            thread(start = true) {
+                // Heartbeat thread
+                while (true) {
+                    val curTime = Date().time
+                    val lastPingTime = AppData.foundServersTimestamps[entry.serverID] ?: break
+
+                    if (curTime - lastPingTime > 5000) {
+                        Log.d(javaClass.name, "${entry.serverID} did not send broadcast in more than 5 seconds, assuming it's down...")
+                        AppData.foundServersTimestamps.remove(entry.serverID)
+                        AppData.foundServers.remove(entry.serverID)
+
+                        AppData.serverAdapter.updateData()
+
+                        if (!AppData.connectLock)
+                            connectSignal.dispatcher.onEvent(Connector.WiFi, ConnectorEvent.CONNECTED_OR_READY)
+
+                        break
+                    }
+
+                    AppData.serverAdapter.updateData()
+                    Thread.sleep(1000)
+                }
+            }
         } else {
             // Second time we received broadcast from this server,
             // if it's the only one - connect to it otherwise show select server button
 
-            if (AppData.foundServers.size == 1)
-                connectTo(Connector.WiFi, "${entry.serverIP}:${entry.serverPort}")
+            if (AppData.foundServers.size == 1 && autoConnectWifi)
+            {
+                Log.d(javaClass.name, "Received broadcast packet from ${entry.serverID}, trying to connect because it's the only one server that sent broadcast...")
+                connectTo(Connector.WiFi, entry.serverIP)
+            }
             else
+            {
+                Log.d(javaClass.name, "Received broadcast packet from ${entry.serverID}, manual connection is needed because either there's more than one server in network or error happened during initial connection...")
                 connectSignal.dispatcher.onEvent(Connector.WiFi, ConnectorEvent.NEED_MANUAL_LAUNCH)
+            }
         }
     }
 
