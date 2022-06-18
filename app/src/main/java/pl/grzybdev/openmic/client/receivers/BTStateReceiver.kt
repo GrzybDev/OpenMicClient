acda1182b3f730fe6dd93629e70215fcdf1eb2a8
Marek Grzyb <grzybdev@gmail.com>
2022-06-18 23:37:56 +0200

    Added BTStateReceiver

---
+++ app/src/main/java/pl/grzybdev/openmic/client/receivers/BTStateReceiver.kt
@@ -0,0 +1,58 @@
+package pl.grzybdev.openmic.client.receivers
+
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.core.app.ActivityCompat
+import com.gazman.signals.Signals
+import pl.grzybdev.openmic.client.AppData
+import pl.grzybdev.openmic.client.OpenMic
+import pl.grzybdev.openmic.client.dataclasses.ServerEntry
+import pl.grzybdev.openmic.client.enumerators.Connector
+import pl.grzybdev.openmic.client.enumerators.ConnectorEvent
+import pl.grzybdev.openmic.client.enumerators.ServerCompatibility
+import pl.grzybdev.openmic.client.enumerators.ServerOS
+import pl.grzybdev.openmic.client.interfaces.IConnector
+
+class BTStateReceiver : BroadcastReceiver() {
+
+    private val connectSignal = Signals.signal(IConnector::class)
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+        when (intent?.action) {
+            BluetoothDevice.ACTION_FOUND -> run {
+                val device: BluetoothDevice? = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+
+                if (OpenMic.App.mainActivity?.applicationContext?.let {
+                        ActivityCompat.checkSelfPermission(
+                            it,
+                            Manifest.permission.BLUETOOTH_CONNECT
+                        )
+                    } != PackageManager.PERMISSION_GRANTED
+                ) {
+                    Log.d(javaClass.name, "App doesn't have BLUETOOTH_CONNECT permission, ignoring onReceive signal!")
+                    return
+                }
+
+                if (device?.name != null) {
+                    Log.d(javaClass.name, "Found Bluetooth device: ${device.name}")
+                    AppData.foundServers[device.address] = ServerEntry(device.name, device.address, ServerCompatibility.UNKNOWN, ServerOS.OTHER, device.address)
+                    connectSignal.dispatcher.onEvent(Connector.Bluetooth, ConnectorEvent.NEED_MANUAL_LAUNCH)
+                }
+            }
+
+            BluetoothAdapter.ACTION_STATE_CHANGED -> run {
+                when (intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, -1)) {
+                    BluetoothAdapter.STATE_OFF -> connectSignal.dispatcher.onEvent(Connector.Bluetooth, ConnectorEvent.DISABLED)
+                    BluetoothAdapter.STATE_ON -> connectSignal.dispatcher.onEvent(Connector.Bluetooth, ConnectorEvent.CONNECTED_OR_READY)
+                }
+            }
+        }
+    }
+
+}
