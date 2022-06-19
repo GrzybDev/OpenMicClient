8026766418fa15bacfc5e0d45e04a2fdfb44eb80
Marek Grzyb <grzybdev@gmail.com>
2022-06-19 11:30:15 +0200

    Added connector reference to ServerEntry

--- app/src/main/java/pl/grzybdev/openmic/client/dataclasses/ServerEntry.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/dataclasses/ServerEntry.kt
@@ -1,6 +1,7 @@
 package pl.grzybdev.openmic.client.dataclasses
 
+import pl.grzybdev.openmic.client.enumerators.Connector
 import pl.grzybdev.openmic.client.enumerators.ServerCompatibility
 import pl.grzybdev.openmic.client.enumerators.ServerOS
 
-data class ServerEntry(val serverName: String, val serverIP: String, val serverCompat: ServerCompatibility, val serverOS: ServerOS, val serverID: String)
+data class ServerEntry(val serverName: String, val serverIP: String, val serverCompat: ServerCompatibility, val serverOS: ServerOS, val serverID: String, val connector: Connector)
