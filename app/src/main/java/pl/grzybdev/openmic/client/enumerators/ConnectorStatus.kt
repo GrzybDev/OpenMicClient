33724643560983176272c90fc61414757f9ceb3a
Marek Grzyb <grzybdev@gmail.com>
2022-08-04 22:21:57 +0200

    Renamed ConnectorEvent to ConnectorStatus

--- app/src/main/java/pl/grzybdev/openmic/client/enumerators/ConnectorStatus.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/enumerators/ConnectorStatus.kt
@@ -1,9 +1,11 @@
 package pl.grzybdev.openmic.client.enumerators
 
-enum class ConnectorEvent {
+enum class ConnectorStatus {
+    UNKNOWN,
     USB_NOT_CONNECTED,
     USB_CONNECTED,
     USB_CHECKING,
+    USB_CONNECTED_NO_SERVER,
     CONNECTING,
     READY,
     DISABLED,
