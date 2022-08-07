ff0c5c106f76f0442493d830fbbd44f5a343e082
Marek Grzyb <grzybdev@gmail.com>
2022-08-07 10:31:33 +0200

    Added forceClose function in Listener

--- app/src/main/java/pl/grzybdev/openmic/client/network/Listener.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/network/Listener.kt
@@ -1,39 +1,44 @@
 package pl.grzybdev.openmic.client.network
 
-import android.content.Context
-import com.gazman.signals.Signals
 import okhttp3.Response
 import okhttp3.WebSocket
 import okhttp3.WebSocketListener
-import pl.grzybdev.openmic.client.OpenMic
 import pl.grzybdev.openmic.client.enumerators.Connector
-import pl.grzybdev.openmic.client.enumerators.ConnectorStatus
-import pl.grzybdev.openmic.client.interfaces.IConnector
 
 class Listener(private val connector: Connector) : WebSocketListener() {
 
-    lateinit var context: Context
-    var connectSignal = Signals.signal(IConnector::class)
+    private var socket: WebSocket? = null
 
     override fun onOpen(webSocket: WebSocket, response: Response) {
+        socket = webSocket
+
         //context?.client?.onOpen(webSocket)
     }
 
     override fun onMessage(webSocket: WebSocket, text: String) {
+        socket = webSocket
+
         //context?.client?.onMessage(webSocket, text)
     }
 
     override fun onClosing(webSocket: WebSocket, code: Int, reason: String) {
+        socket = webSocket
+
         handleDisconnect()
     }
 
     override fun onFailure(webSocket: WebSocket, t: Throwable, response: Response?) {
+        socket = webSocket
         handleDisconnect()
-        connectSignal.dispatcher.onEvent(connector, ConnectorStatus.NEED_MANUAL_LAUNCH)
     }
 
     private fun handleDisconnect()
     {
         //context?.client?.handleDisconnect()
+    }
+
+    fun forceClose()
+    {
+        socket?.close(1000, "")
     }
 }
