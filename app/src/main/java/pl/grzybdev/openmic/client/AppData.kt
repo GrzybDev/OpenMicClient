362c12f4fff1643ca455d0e0e01d4e3f13e59b67
Marek Grzyb <grzybdev@gmail.com>
2022-08-07 10:29:49 +0200

    Updated AppData object

--- app/src/main/java/pl/grzybdev/openmic/client/AppData.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/AppData.kt
@@ -1,11 +1,19 @@
 package pl.grzybdev.openmic.client
 
 import com.gazman.signals.Signals
+import pl.grzybdev.openmic.client.enumerators.ConnectionStatus
+import pl.grzybdev.openmic.client.enumerators.ConnectorStatus
 import pl.grzybdev.openmic.client.interfaces.IConnector
+import java.util.TimerTask
 
 object AppData {
+    val openmic: OpenMic = OpenMic()
 
     var connectSignal = Signals.signal(IConnector::class)
+    var connectionStatus = ConnectionStatus.UNKNOWN
+
+    var usbStatus: ConnectorStatus = ConnectorStatus.UNKNOWN
+    var usbTimer: TimerTask? = null
 
     var communicationPort = 10000
     var deviceID: String = ""
