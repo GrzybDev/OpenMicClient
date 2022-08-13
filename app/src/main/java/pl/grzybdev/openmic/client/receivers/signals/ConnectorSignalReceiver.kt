2c8ff534f916c941139f9f4d1d2ded53778c29d9
Marek Grzyb <grzybdev@gmail.com>
2022-08-13 14:11:35 +0200

    Added ConnectorSignalReceiver

---
+++ app/src/main/java/pl/grzybdev/openmic/client/receivers/signals/ConnectorSignalReceiver.kt
@@ -0,0 +1,64 @@
+package pl.grzybdev.openmic.client.receivers.signals
+
+import android.content.BroadcastReceiver
+import android.util.Log
+import pl.grzybdev.openmic.client.enumerators.Connector
+import pl.grzybdev.openmic.client.enumerators.ConnectorState
+import pl.grzybdev.openmic.client.interfaces.IConnector
+import pl.grzybdev.openmic.client.singletons.AppData
+
+class ConnectorSignalReceiver : BroadcastReceiver() {
+
+    override fun onReceive(context: android.content.Context, intent: android.content.Intent) {
+        if (intent.action == "UpdateState") {
+            val connectorInt: Int = intent.getIntExtra("connector", -1)
+            val connector: Connector = Connector.values()[connectorInt]
+
+            val stateInt: Int = intent.getIntExtra("state", -1)
+            val state: ConnectorState = ConnectorState.values()[stateInt]
+
+            Log.d(javaClass.name, "Connector: $connector, State: $state")
+
+            when (connector) {
+                Connector.USB -> {
+                    if (state == ConnectorState.USB_CONNECTED || state == ConnectorState.USB_CHECKING)
+                    {
+                        if (AppData.usbState == ConnectorState.READY
+                            || AppData.usbState == ConnectorState.USB_CONNECTED_NO_SERVER)
+                            return
+                    }
+
+                    AppData.usbState = state
+                }
+                else -> {}
+            }
+
+            notifyAllListeners(connector, state)
+        }
+    }
+
+    private fun notifyAllListeners(connector: Connector, state: ConnectorState)
+    {
+        for (listener in AppData.connectorListeners)
+        {
+            when (connector)
+            {
+                Connector.USB -> {
+                    listener.onUSBStateChange(state)
+                }
+                else -> {}
+            }
+        }
+    }
+
+    fun addListener(listener: IConnector)
+    {
+        AppData.connectorListeners.add(listener)
+    }
+
+    fun removeListener(listener: IConnector)
+    {
+        AppData.connectorListeners.remove(listener)
+    }
+
+}
