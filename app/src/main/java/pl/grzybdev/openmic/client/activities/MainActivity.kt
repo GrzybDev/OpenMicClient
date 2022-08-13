2947a7461ce9367c204bbe5f1c270cdbb94788c0
Marek Grzyb <grzybdev@gmail.com>
2022-08-13 20:34:10 +0200

    Finished Dialog support in MainActivity

--- app/src/main/java/pl/grzybdev/openmic/client/activities/MainActivity.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/activities/MainActivity.kt
@@ -7,7 +7,10 @@ import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.text.Editable
 import android.text.InputType
+import android.text.TextUtils
+import android.text.TextWatcher
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
@@ -29,17 +32,20 @@ import pl.grzybdev.openmic.client.databinding.ActivityMainBinding
 import pl.grzybdev.openmic.client.enumerators.ConnectionStatus
 import pl.grzybdev.openmic.client.enumerators.DialogType
 import pl.grzybdev.openmic.client.interfaces.IConnection
+import pl.grzybdev.openmic.client.interfaces.IDialog
+import pl.grzybdev.openmic.client.network.messages.client.AuthCodeVerify
 import pl.grzybdev.openmic.client.receivers.signals.ConnectionSignalReceiver
+import pl.grzybdev.openmic.client.receivers.signals.DialogSignalReceiver
 import pl.grzybdev.openmic.client.singletons.AppData
 import java.util.*
 
-class MainActivity : AppCompatActivity(), IConnection {
+class MainActivity : AppCompatActivity(), IConnection, IDialog {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     private var connectionSignal: ConnectionSignalReceiver = ConnectionSignalReceiver()
-    private var dialogListener = { t: DialogType, d: Any? -> spawnDialog(t, d) }
+    private var dialogSignal: DialogSignalReceiver = DialogSignalReceiver()
 
     private lateinit var dialog: AlertDialog
 
@@ -59,6 +65,7 @@ class MainActivity : AppCompatActivity(), IConnection {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         sharedPrefs = getSharedPreferences(getString(R.string.PREFERENCE_APP), MODE_PRIVATE)
+        AppData.sharedPrefs = sharedPrefs
 
         val deviceIDKey = getString(R.string.PREFERENCE_APP_DEVICE_ID)
 
@@ -72,7 +79,9 @@ class MainActivity : AppCompatActivity(), IConnection {
             }
         }
 
+        AppData.deviceID = sharedPrefs.getString(deviceIDKey, "INVALID").toString()
         AppData.resources = resources
+
         dialog = AlertDialog.Builder(this).create()
 
         if (savedInstanceState == null) {
@@ -86,8 +95,8 @@ class MainActivity : AppCompatActivity(), IConnection {
         connectionSignal.addListener(this)
         registerReceiver(connectionSignal, IntentFilter("UpdateStatus"))
 
-        // AppData.connectionSignal.addListener(connectionListener)
-        // AppData.dialogSignal.addListener(dialogListener)
+        dialogSignal.addListener(this)
+        registerReceiver(connectionSignal, IntentFilter("ShowDialog"))
     }
 
     override fun onDestroy() {
@@ -96,7 +105,8 @@ class MainActivity : AppCompatActivity(), IConnection {
         connectionSignal.removeListener(this)
         unregisterReceiver(connectionSignal)
 
-        // AppData.dialogSignal.removeListener(dialogListener)
+        dialogSignal.removeListener(this)
+        unregisterReceiver(dialogSignal)
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -173,57 +183,6 @@ class MainActivity : AppCompatActivity(), IConnection {
         }
     }
 
-    private fun spawnDialog(type: DialogType, data: Any?)
-    {
-        runOnUiThread {
-            if (dialog.isShowing)
-                dialog.dismiss()
-
-            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-
-            when (type) {
-                DialogType.SERVER_ERROR -> {
-                    builder.setTitle(getString(R.string.dialog_srverr_title))
-                    builder.setMessage(data as String)
-                    builder.setPositiveButton(getString(R.string.dialog_srverr_btn_ok)) {
-                        _, _ ->
-                    }
-                }
-
-                DialogType.SERVER_DISCONNECT -> {
-
-                }
-
-                DialogType.AUTH -> {
-                    builder.setTitle(getString(R.string.dialog_auth_title))
-
-                    // Set up the input
-                    val input = EditText(this)
-                    input.inputType = InputType.TYPE_CLASS_NUMBER
-                    builder.setView(input)
-
-                    // Set up the buttons
-                    builder.setPositiveButton(getString(R.string.dialog_auth_btn_ok)) { _, _ ->
-                        run {
-                            val authCode = Integer.parseInt(input.text.toString())
-
-                            Log.d(javaClass.name, "Entered code: $authCode")
-                        }
-                    }
-
-                    builder.setNegativeButton(getString(R.string.dialog_auth_btn_cancel)) { _, _ ->
-                        run {
-                            Log.d(javaClass.name, "Canceled auth dialog! Disconnecting...")
-                        }
-                    }
-                }
-            }
-
-            val dialog = builder.create()
-            dialog.show()
-        }
-    }
-
     override fun onConnectionStateChange(status: ConnectionStatus) {
         runOnUiThread {
             val navController: NavController
@@ -255,6 +214,108 @@ class MainActivity : AppCompatActivity(), IConnection {
                 ConnectionStatus.DISCONNECTED -> {
                     navController.navigate(R.id.action_disconnected)
                     OpenMic.changeConnectionStatus(this, ConnectionStatus.NOT_CONNECTED)
+                }
+            }
+        }
+    }
+
+    override fun showDialog(type: DialogType, data: String?) {
+        runOnUiThread {
+            if (dialog.isShowing && type != DialogType.SERVER_ERROR)
+                dialog.dismiss()
+
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+
+            when (type) {
+                DialogType.SERVER_ERROR -> {
+                    builder.setTitle(getString(R.string.dialog_srverr_title))
+                    builder.setMessage(data)
+                    builder.setPositiveButton(getString(R.string.dialog_srverr_btn_ok)) {
+                            _, _ ->
+                    }
+                }
+
+                DialogType.SERVER_DISCONNECT -> {
+                    builder.setTitle(getString(R.string.dialog_disconnect_title))
+
+                    if (data == "")
+                        builder.setMessage(getString(R.string.dialog_disconnect_no_reason))
+                    else
+                        builder.setMessage(data)
+
+                    builder.setPositiveButton(getString(R.string.dialog_disconnect_btn_ok)) {
+                            _, _ ->
+                    }
+                }
+
+                DialogType.AUTH -> {
+                    builder.setTitle(getString(R.string.dialog_auth_title))
+
+                    // Set up the input
+                    val input = EditText(this)
+                    input.inputType = InputType.TYPE_CLASS_NUMBER
+                    input.addTextChangedListener(object : TextWatcher {
+                        override fun beforeTextChanged(
+                            s: CharSequence?,
+                            start: Int,
+                            count: Int,
+                            after: Int
+                        ) {
+                            // Not needed
+                        }
+                        override fun onTextChanged(
+                            s: CharSequence?,
+                            start: Int,
+                            before: Int,
+                            count: Int
+                        ) {
+                            // Not needed
+                        }
+
+                        override fun afterTextChanged(s: Editable?) {
+                            // Disable verify button if input is empty
+                            dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = !TextUtils.isEmpty(s)
+                        }
+                    })
+
+                    builder.setView(input)
+
+                    // Set up the buttons
+                    builder.setPositiveButton(getString(R.string.dialog_auth_btn_ok)) { _, _ ->
+                        run {
+                            val authCode = Integer.parseInt(input.text.toString())
+                            Log.d(javaClass.name, "Entered code: $authCode")
+
+                            AppData.openmic.wsClient.sendPacket(AuthCodeVerify(authCode))
+                        }
+                    }
+
+                    builder.setNegativeButton(getString(R.string.dialog_auth_btn_cancel)) { _, _ ->
+                        run {
+                            Log.d(javaClass.name, "Canceled auth dialog! Disconnecting...")
+                            AppData.openmic.forceDisconnect(getString(R.string.dialog_disconnect_auth_dismissed))
+                        }
+                    }
+                }
+            }
+
+            if (type == DialogType.SERVER_ERROR)
+                builder.create().show()
+            else
+            {
+                dialog = builder.create()
+                dialog.show()
+            }
+
+            if (type == DialogType.AUTH)
+            {
+                // Disable verify button (initially)
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).isEnabled = false
+                dialog.setOnCancelListener {
+                    run {
+                        Log.d(javaClass.name, "Auth dialog was dismissed! Disconnecting...")
+                        AppData.openmic.forceDisconnect(getString(R.string.dialog_disconnect_auth_dismissed))
+                    }
                 }
             }
         }
