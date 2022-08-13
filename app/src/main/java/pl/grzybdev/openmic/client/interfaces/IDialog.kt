ba9bb1be0756088c09a003d9fd534e83380bf22a
Marek Grzyb <grzybdev@gmail.com>
2022-08-13 20:31:14 +0200

    Updated IDialog

--- app/src/main/java/pl/grzybdev/openmic/client/interfaces/IDialog.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/interfaces/IDialog.kt
@@ -2,6 +2,6 @@ package pl.grzybdev.openmic.client.interfaces
 
 import pl.grzybdev.openmic.client.enumerators.DialogType
 
-fun interface IDialog {
-    fun onEvent(type: DialogType, data: Any?)
+interface IDialog {
+    fun showDialog(type: DialogType, data: String?)
 }
