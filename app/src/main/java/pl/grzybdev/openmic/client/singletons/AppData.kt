06602291f5f087999f357d585aff8065e245dcb0
Marek Grzyb <grzybdev@gmail.com>
2022-08-13 20:30:27 +0200

    Updated AppData

--- app/src/main/java/pl/grzybdev/openmic/client/singletons/AppData.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/singletons/AppData.kt
@@ -1,19 +1,24 @@
 package pl.grzybdev.openmic.client.singletons
 
+import android.content.SharedPreferences
 import android.content.res.Resources
 import pl.grzybdev.openmic.client.OpenMic
 import pl.grzybdev.openmic.client.enumerators.ConnectionStatus
 import pl.grzybdev.openmic.client.enumerators.ConnectorState
 import pl.grzybdev.openmic.client.interfaces.IConnection
 import pl.grzybdev.openmic.client.interfaces.IConnector
+import pl.grzybdev.openmic.client.interfaces.IDialog
 import java.util.*
 
 object AppData {
     var openmic: OpenMic = OpenMic()
+
+    var sharedPrefs: SharedPreferences? = null
     var resources: Resources? = null
 
     var connectorListeners: MutableList<IConnector> = mutableListOf()
     var connectionListeners: MutableList<IConnection> = mutableListOf()
+    var dialogListeners: MutableList<IDialog> = mutableListOf()
 
     var connectionStatus = ConnectionStatus.NOT_CONNECTED
 
