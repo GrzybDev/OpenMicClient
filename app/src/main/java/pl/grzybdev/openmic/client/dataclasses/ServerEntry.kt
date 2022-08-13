1da820ab53393f57868123212ce4cc04931076c9
Marek Grzyb <grzybdev@gmail.com>
2022-08-13 14:16:26 +0200

    Reformatted ServerEntry

--- app/src/main/java/pl/grzybdev/openmic/client/dataclasses/ServerEntry.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/dataclasses/ServerEntry.kt
@@ -1,7 +1,11 @@
 package pl.grzybdev.openmic.client.dataclasses
 
 import pl.grzybdev.openmic.client.enumerators.Connector
-import pl.grzybdev.openmic.client.enumerators.ServerCompatibility
-import pl.grzybdev.openmic.client.enumerators.ServerOS
+import pl.grzybdev.openmic.client.enumerators.ServerVersion
 
-data class ServerEntry(val serverName: String, val serverIP: String, val serverCompat: ServerCompatibility, val serverOS: ServerOS, val serverID: String, val connector: Connector)
+data class ServerEntry(val serverName: String,
+                       val serverIP: String,
+                       val serverCompat: ServerVersion,
+                       val serverOS: String,
+                       val serverID: String,
+                       val connector: Connector)
