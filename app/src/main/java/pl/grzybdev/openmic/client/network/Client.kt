7fd646b6188c24e68528caf00e8a805d5008a3d6
Marek Grzyb <grzybdev@gmail.com>
2022-08-13 14:17:43 +0200

    Reworked Client

--- app/src/main/java/pl/grzybdev/openmic/client/network/Client.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/network/Client.kt
@@ -1,16 +1,15 @@
 package pl.grzybdev.openmic.client.network
 
 import android.bluetooth.BluetoothSocket
+import android.content.Context
 import android.util.Log
-import com.gazman.signals.Signals
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import okhttp3.WebSocket
-import pl.grzybdev.openmic.client.AppData
 import pl.grzybdev.openmic.client.BuildConfig
-import pl.grzybdev.openmic.client.dialogs.DialogShared
+import pl.grzybdev.openmic.client.OpenMic
+import pl.grzybdev.openmic.client.enumerators.ConnectionStatus
 import pl.grzybdev.openmic.client.enumerators.Connector
-import pl.grzybdev.openmic.client.interfaces.IError
 import pl.grzybdev.openmic.client.network.messages.ErrorCode
 import pl.grzybdev.openmic.client.network.messages.Message
 import pl.grzybdev.openmic.client.network.messages.client.ClientPacket
@@ -18,14 +17,11 @@ import pl.grzybdev.openmic.client.network.messages.client.SystemHello
 import pl.grzybdev.openmic.client.network.messages.server.BasePacket
 import pl.grzybdev.openmic.client.network.messages.server.ErrorPacket
 import pl.grzybdev.openmic.client.network.messages.server.ServerPacket
+import pl.grzybdev.openmic.client.singletons.AppData
 
-class Client(private val connector: Connector?) {
-
-    var isConnected: Boolean = false
+class Client(val context: Context?, private val connector: Connector?) {
 
     fun onOpen(socket: Any) {
-        isConnected = true
-
         val packet: ClientPacket = SystemHello(BuildConfig.APPLICATION_ID, BuildConfig.VERSION_NAME, AppData.deviceID)
 
         if (connector != Connector.Bluetooth) {
@@ -46,64 +42,41 @@ class Client(private val connector: Connector?) {
 
             if (mType != null) {
                 if (connector != null) {
-                    Handler.handlePacket(socket, connector, mType, text)
+                    context?.let { Handler.handlePacket(it, socket, connector, mType, text) }
                 }
             } else {
-                Log.e(javaClass.name, "Unknown message type! ($mType) Disconnecting...")
-
-                if (connector != Connector.Bluetooth) {
-                    val webSocket = socket as WebSocket
-                    webSocket.close(1003, "Unknown message type")
-                } else {
-                    val btSocket = socket as BluetoothSocket
-                    btSocket.close()
-                }
+                Log.w(javaClass.name, "Unknown message type! ($mType) Disconnecting...")
+                handleDisconnect(socket, 1003)
             }
         } else {
             Log.e(javaClass.name, "Received error packet, showing dialog...")
             val packet = pBase as ErrorPacket
-            val errorSignal = Signals.signal(IError::class)
+            // val dialogSignal = Signals.signal(IDialog::class)
             val errorType: ErrorCode? = ErrorCode.values().find { it.code == packet.error }
 
-            if (errorType == null) {
-                Log.e(javaClass.name, "Unknown error code! (${packet.error}), disconnecting...")
-
-                if (connector != Connector.Bluetooth) {
-                    val webSocket = socket as WebSocket
-                    webSocket.close(1003, "Unknown error code")
-                } else {
-                    val btSocket = socket as BluetoothSocket
-                    btSocket.close()
-                }
-
-                return
+            if (errorType != null) {
+                // dialogSignal.dispatcher.onEvent(DialogType.SERVER_ERROR, packet.message)
+            } else {
+                handleDisconnect(socket)
             }
-
-            errorSignal.dispatcher.onErrorMessage(errorType)
-
-            /*
-            OpenMic.App.mainActivity?.runOnUiThread {
-                val builder: AlertDialog.Builder = AlertDialog.Builder(OpenMic.App.mainActivity)
-                builder.setTitle(OpenMic.App.mainActivity?.getString(R.string.ErrorDialog_Title))
-                builder.setMessage(packet.message)
-                builder.setPositiveButton(OpenMic.App.mainActivity?.getString(R.string.ErrorDialog_Button_OK)) { _, _ -> AppData.connectLock = false }
-                builder.show()
-            }
-
-             */
         }
     }
 
-    fun handleDisconnect()
+    fun handleDisconnect(socket: Any, code: Int = 1000)
     {
-        isConnected = false
-        // AppData.connectLock = false
+        if (context == null) { return }
 
-        DialogShared.current?.dismiss()
-        // OpenMic.App.mainActivity?.onDisconnect()
-        Audio.stop()
+        OpenMic.changeConnectionStatus(context, ConnectionStatus.DISCONNECTING)
 
-        // bOpenMic.App.context?.initClient()
+        if (connector != Connector.Bluetooth) {
+            val webSocket = socket as WebSocket
+            webSocket.close(code, null)
+        } else {
+            val btSocket = socket as BluetoothSocket
+            btSocket.close()
+        }
+
+        OpenMic.changeConnectionStatus(context, ConnectionStatus.DISCONNECTED)
     }
 
 }
