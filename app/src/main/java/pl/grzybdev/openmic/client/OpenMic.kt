1daae5c7b5ef0157e636cf5475d557a7483e3881
Marek Grzyb <grzybdev@gmail.com>
2022-08-13 14:19:57 +0200

    Optimized OpenMic

--- app/src/main/java/pl/grzybdev/openmic/client/OpenMic.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/OpenMic.kt
@@ -1,20 +1,29 @@
 package pl.grzybdev.openmic.client
 
 import android.content.Context
+import android.content.Intent
 import android.util.Log
-import androidx.navigation.NavController
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.WebSocketListener
 import pl.grzybdev.openmic.client.enumerators.ConnectionStatus
 import pl.grzybdev.openmic.client.enumerators.Connector
-import pl.grzybdev.openmic.client.enumerators.ConnectorStatus
+import pl.grzybdev.openmic.client.enumerators.ConnectorState
+import pl.grzybdev.openmic.client.enumerators.ServerVersion
+import pl.grzybdev.openmic.client.network.Client
 import pl.grzybdev.openmic.client.network.Listener
 import pl.grzybdev.openmic.client.network.USBCheckListener
+import pl.grzybdev.openmic.client.receivers.signals.ConnectionSignalReceiver
+import pl.grzybdev.openmic.client.receivers.signals.ConnectorSignalReceiver
+import pl.grzybdev.openmic.client.singletons.AppData
+import java.util.*
 import java.util.concurrent.TimeUnit
+import kotlin.concurrent.schedule
 
 
 class OpenMic {
+
+    var wsClient = Client(null, null)
 
     private val httpClient = OkHttpClient.Builder()
         .readTimeout(20, TimeUnit.SECONDS)
@@ -23,73 +32,96 @@ class OpenMic {
 
     private var listener: WebSocketListener? = null
 
-    fun connectTo(connector: Connector, address: String) {
+    companion object {
+        fun changeConnectionStatus(ctx: Context, status: ConnectionStatus)
+        {
+            val i = Intent(ctx, ConnectionSignalReceiver::class.java)
+            i.action = "UpdateStatus"
+            i.putExtra("status", status.ordinal)
+            ctx.sendBroadcast(i)
+        }
+
+        fun changeConnectorStatus(ctx: Context, connector: Connector, status: ConnectorState)
+        {
+            val i = Intent(ctx, ConnectorSignalReceiver::class.java)
+            i.action = "UpdateState"
+            i.putExtra("connector", connector.ordinal)
+            i.putExtra("state", status.ordinal)
+            ctx.sendBroadcast(i)
+        }
+    }
+
+    fun connectTo(ctx: Context, connector: Connector, address: String) {
         if (AppData.connectionStatus >= ConnectionStatus.CONNECTING && AppData.connectionStatus <= ConnectionStatus.CONNECTED) {
             // It should never execute as user can connect only on start screen, and when user is connected or connecting start screen is inaccessible
             Log.wtf(javaClass.name, "Can't connect because connection status is ${AppData.connectionStatus} which is illegal at this stage. How did you get here?")
             return
         }
 
-        AppData.connectionStatus = ConnectionStatus.CONNECTING
+        changeConnectionStatus(ctx, ConnectionStatus.CONNECTING)
+        changeConnectorStatus(ctx, connector, ConnectorState.UNKNOWN)
 
-        forceDisconnect(null)
+        forceDisconnect()
 
+        wsClient = Client(ctx, connector)
         Log.d(javaClass.name, "connectTo: Trying to connect to $address, via $connector...")
 
         if (connector != Connector.Bluetooth) {
-            listener = Listener(connector)
+            listener = Listener()
 
             val webRequest = Request.Builder()
                 .url("ws://$address:${AppData.communicationPort}")
                 .build()
 
             httpClient.newWebSocket(webRequest, listener as Listener)
-            httpClient.dispatcher.executorService.shutdown()
         } else {
         // val client = Client(connector)
         }
     }
 
-    fun usbCheck(ctx: Context) {
+    fun usbCheck(ctx: Context, uiDelay: Boolean = false) {
         Log.d(javaClass.name, "usbCheck: Checking if USB device has OpenMic Server running...")
 
-        if (AppData.usbStatus != ConnectorStatus.READY
-            && AppData.usbStatus != ConnectorStatus.USB_CONNECTED_NO_SERVER)
-            // Don't update status if it's already set to READY or USB_CONNECTED_NO_SERVER
-            AppData.connectSignal.dispatcher.onEvent(Connector.USB, ConnectorStatus.USB_CHECKING)
+        changeConnectorStatus(ctx, Connector.USB, ConnectorState.USB_CHECKING)
 
         listener = USBCheckListener(ctx)
 
-        val httpClient = OkHttpClient.Builder()
-            .readTimeout(20, TimeUnit.SECONDS)
-            .pingInterval(20, TimeUnit.SECONDS)
-            .build()
-
         val webRequest = Request.Builder()
-            .url("ws://${ctx.getString(R.string.INTERNAL_USB_ADDRESS)}:${AppData.communicationPort}")
+            .url("ws://${AppData.resources?.getString(R.string.INTERNAL_USB_ADDRESS)}:${AppData.communicationPort}")
             .build()
 
-        httpClient.newWebSocket(webRequest, listener as USBCheckListener)
-        httpClient.dispatcher.executorService.shutdown()
+        if (uiDelay) {
+            Timer("USBCheckDelay", false).schedule(5000) {
+                httpClient.newWebSocket(webRequest, listener as USBCheckListener)
+            }
+        } else {
+            httpClient.newWebSocket(webRequest, listener as USBCheckListener)
+        }
     }
 
-    fun forceDisconnect(navController: NavController?) {
+    fun forceDisconnect() {
         Log.d(javaClass.name, "forceDisconnect: Disconnecting...")
-
-        navController?.navigate(R.id.action_cancel_connection)
 
         if (listener != null)
         {
             if (listener is USBCheckListener)
                 (listener as USBCheckListener).forceClose()
             else
-                (listener as Listener).forceClose()
+                (listener as Listener).handleDisconnect()
         }
         else
-        {
             Log.w(javaClass.name, "forceDisconnect: No listener to close...")
+    }
+
+    fun getServerVersion(serverApp: String, serverVersion: String): ServerVersion {
+        if (serverApp == AppData.resources?.getString(R.string.SERVER_APP_NAME)) {
+            // It's official app, check if versions match
+            if (serverVersion != BuildConfig.VERSION_NAME)
+                return ServerVersion.MISMATCH
+
+            return ServerVersion.MATCH
         }
 
-        navController?.navigate(R.id.action_disconnected)
+        return ServerVersion.UNOFFICIAL
     }
 }
