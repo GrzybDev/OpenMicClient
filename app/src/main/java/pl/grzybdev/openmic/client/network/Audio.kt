f3e4cccb941544eba9c8fe95022aa61d416ca308
Marek Grzyb <grzybdev@gmail.com>
2022-08-19 20:51:20 +0200

    Started rework of Audio service

--- app/src/main/java/pl/grzybdev/openmic/client/network/Audio.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/network/Audio.kt
@@ -2,40 +2,47 @@ package pl.grzybdev.openmic.client.network
 
 import android.media.AudioFormat
 import android.media.AudioRecord
-import pl.grzybdev.openmic.client.enumerators.Connector
+import pl.grzybdev.openmic.client.R
+import pl.grzybdev.openmic.client.enumerators.audio.Channels
+import pl.grzybdev.openmic.client.enumerators.audio.Format
+import pl.grzybdev.openmic.client.network.messages.client.StreamStart
+import pl.grzybdev.openmic.client.singletons.AppData
+import pl.grzybdev.openmic.client.singletons.StreamData
 
 
 class Audio {
+    private lateinit var socket: Any
 
-    object Data {
-        var socket: Any? = null
-        var connector: Connector? = null
+    fun start(socket: Any) {
+        this.socket = socket
 
-        var sampleRate = 8000
-        var channelConfig = AudioFormat.CHANNEL_IN_MONO
-        var audioFormat = AudioFormat.ENCODING_PCM_16BIT
-        var minBufSize = AudioRecord.getMinBufferSize(sampleRate, channelConfig, audioFormat)
+        StreamData.sampleRate = AppData.sharedPrefs?.getInt(AppData.resources?.getString(R.string.PREFERENCE_APP_AUDIO_SAMPLE_RATE), 8000)!!
+        val channels = AppData.sharedPrefs?.getInt(AppData.resources?.getString(R.string.PREFERENCE_APP_AUDIO_CHANNELS), AudioFormat.CHANNEL_IN_MONO)!!
+        val format = AppData.sharedPrefs?.getInt(AppData.resources?.getString(R.string.PREFERENCE_APP_AUDIO_FORMAT), AudioFormat.ENCODING_PCM_8BIT)!!
 
-        //var intent = Intent(OpenMic.App.mainActivity, AudioService::class.java)
-    }
+        StreamData.bufferSize = AudioRecord.getMinBufferSize(StreamData.sampleRate, channels, format)
 
-    companion object {
+        if (StreamData.bufferSize == AudioRecord.ERROR || StreamData.bufferSize == AudioRecord.ERROR_BAD_VALUE) {
+            // 44100Hz is currently the only rate that is guaranteed to work on all devices
+            StreamData.sampleRate = 44100
+            StreamData.bufferSize = AudioRecord.getMinBufferSize(StreamData.sampleRate, channels, format)
 
-        fun start(socket: Any?, connector: Connector?) {
-            if (socket != null && connector != null) {
-                Data.socket = socket
-                Data.connector = connector
+            with (AppData.sharedPrefs?.edit()) {
+                this?.putInt(AppData.resources?.getString(R.string.PREFERENCE_APP_AUDIO_SAMPLE_RATE), StreamData.sampleRate)
+                this?.apply()
             }
-
-            // val signal = Signals.signal(IConnector::class)
-            //AppData.currentConn?.let { signal.dispatcher.onEvent(it, ConnectorStatus.CONNECTED_OR_READY) }
-
-            //ContextCompat.startForegroundService(OpenMic.App.mainActivity!!, Data.intent)
         }
 
-        fun stop() {
-            //OpenMic.App.mainActivity?.stopService(Data.intent)
+        StreamData.channels = if (channels == AudioFormat.CHANNEL_IN_MONO) Channels.MONO else Channels.STEREO
+
+        StreamData.format = when (format) {
+            AudioFormat.ENCODING_PCM_8BIT -> Format.PCM_8BIT
+            AudioFormat.ENCODING_PCM_16BIT -> Format.PCM_16BIT
+            AudioFormat.ENCODING_PCM_FLOAT -> Format.PCM_FLOAT
+            else -> Format.INVALID
         }
 
+        AppData.openmic.client.sendPacket(StreamStart(StreamData.sampleRate, StreamData.channels.ordinal, StreamData.format.ordinal))
     }
+
 }
