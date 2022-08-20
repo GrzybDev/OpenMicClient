1ed72c7e2e7ad9b7720078ffc09b5252cf2ad8cc
Marek Grzyb <grzybdev@gmail.com>
2022-08-20 16:10:26 +0200

    Handle handling errors correctly

--- app/src/main/java/pl/grzybdev/openmic/client/network/Handler.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/network/Handler.kt
@@ -1,10 +1,13 @@
 package pl.grzybdev.openmic.client.network
 
 import android.content.Context
+import android.util.Log
 import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.json.JsonContentPolymorphicSerializer
 import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.jsonObject
+import pl.grzybdev.openmic.client.OpenMic
+import pl.grzybdev.openmic.client.enumerators.DialogType
 import pl.grzybdev.openmic.client.enumerators.network.Connector
 import pl.grzybdev.openmic.client.network.messages.Message
 import pl.grzybdev.openmic.client.network.messages.server.*
@@ -28,14 +31,22 @@ class Handler {
         }
 
         fun handlePacket(context: Context, socket: Any, connector: Connector, type: Message, data: String) {
-            when (type) {
-                Message.SYSTEM_HELLO, Message.SYSTEM_GOODBYE, Message.SYSTEM_IS_ALIVE -> SystemPacket.handle(context, socket, connector, type, data)
-                Message.AUTH_CLIENT, Message.AUTH_CODE_VERIFY -> AuthPacket.handle(
-                    context,
-                    socket,
-                    type,
-                    data
-                )
+            try {
+                when (type) {
+                    Message.SYSTEM_HELLO, Message.SYSTEM_GOODBYE, Message.SYSTEM_IS_ALIVE -> SystemPacket.handle(context, socket, connector, type, data)
+                    Message.AUTH_CLIENT, Message.AUTH_CODE_VERIFY -> AuthPacket.handle(
+                        context,
+                        socket,
+                        type,
+                        data
+                    )
+                    Message.STREAM_START -> StreamPacket.handle(context, type, data)
+                }
+            }
+            catch (e: Exception)
+            {
+                Log.e(Handler::class.java.name, e.message.toString())
+                OpenMic.showDialog(context, DialogType.CLIENT_INTERNAL_ERROR, null)
             }
         }
     }
