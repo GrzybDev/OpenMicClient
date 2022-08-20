7e6885d6e4fe451173e0b80a2569d6a5e6e4dae9
Marek Grzyb <grzybdev@gmail.com>
2022-08-20 16:13:27 +0200

    Reorganized StreamData singleton

--- app/src/main/java/pl/grzybdev/openmic/client/singletons/StreamData.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/singletons/StreamData.kt
@@ -1,11 +1,20 @@
 package pl.grzybdev.openmic.client.singletons
 
+import android.content.Intent
 import pl.grzybdev.openmic.client.enumerators.audio.Channels
 import pl.grzybdev.openmic.client.enumerators.audio.Format
 
 object StreamData {
     var sampleRate: Int = -1
+
     var channels: Channels = Channels.INVALID
+    var channelsInt: Int = -1
+
     var format: Format = Format.INVALID
+    var formatInt: Int = -1
+
     var bufferSize: Int = -1
+
+    var intent: Intent = Intent()
+    var intentActive: Boolean = false
 }
