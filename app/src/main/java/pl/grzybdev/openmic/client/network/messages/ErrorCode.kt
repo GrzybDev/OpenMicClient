c4115ab8544cba5c3097a1f37808bcf3764d245e
Marek Grzyb <grzybdev@gmail.com>
2022-08-20 16:10:51 +0200

    Synchronized ErrorCodes with server

--- app/src/main/java/pl/grzybdev/openmic/client/network/messages/ErrorCode.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/network/messages/ErrorCode.kt
@@ -4,5 +4,7 @@ enum class ErrorCode(val code: Int) {
     NORMAL(0),
     VERSION_MISMATCH(1),
     CANCELED_AUTH_CODE_DIALOG(2),
-    AUTH_CODE_INVALID(3)
+    AUTH_CODE_INVALID(3),
+    SERVER_RESTARTING(4),
+    NOT_ACCEPTABLE_CONFIG(5)
 }
