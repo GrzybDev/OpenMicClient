dd193019fdf0096c9173ce2dc4ff0586afeff008
Marek Grzyb <grzybdev@gmail.com>
2022-08-28 22:13:15 +0200

    Improved service notification

--- app/src/main/java/pl/grzybdev/openmic/client/enumerators/audio/Action.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/enumerators/audio/Action.kt
@@ -3,4 +3,6 @@ package pl.grzybdev.openmic.client.enumerators.audio
 enum class Action(val code: Int) {
     START(0),
     TOGGLE_MUTE(1),
+    DISCONNECT(2),
+    TOGGLE_MUTE_SELF(3),
 }
--- app/src/main/java/pl/grzybdev/openmic/client/services/AudioService.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/services/AudioService.kt
@@ -10,6 +10,7 @@ import android.os.IBinder
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import pl.grzybdev.openmic.client.BuildConfig
+import pl.grzybdev.openmic.client.OpenMic
 import pl.grzybdev.openmic.client.R
 import pl.grzybdev.openmic.client.activities.MainActivity
 import pl.grzybdev.openmic.client.enumerators.audio.Action
@@ -23,6 +24,8 @@ class AudioService : Service() {
 
     lateinit var audioThread: Thread
     lateinit var recorder: AudioRecord
+
+    private var notificationInitialized = false
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -44,14 +47,15 @@ class AudioService : Service() {
         }
     }
 
-    @SuppressLint("UnspecifiedImmutableFlag")
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         val actionInt = intent.getIntExtra("action", -1)
 
         if (actionInt != -1) {
             when (Action.values().find {it.code == actionInt}) {
                 Action.START -> run { startStream() }
-                Action.TOGGLE_MUTE -> run { toggleMute() }
+                Action.TOGGLE_MUTE -> run { toggleMute(false) }
+                Action.TOGGLE_MUTE_SELF -> run { toggleMute(true) }
+                Action.DISCONNECT -> run { AppData.openmic.forceDisconnect() }
                 else -> {}
             }
 
@@ -61,29 +65,9 @@ class AudioService : Service() {
         return START_STICKY
     }
 
-    @SuppressLint("UnspecifiedImmutableFlag")
     private fun startStream()
     {
-        val notificationIntent = Intent(this, MainActivity::class.java)
-
-        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
-        } else {
-            PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-        }
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
-            createNotificationChannel()
-
-        val notification: Notification =
-            NotificationCompat.Builder(this, BuildConfig.APPLICATION_ID)
-                .setContentTitle(getString(R.string.connected_action_title))
-                .setContentText(getString(R.string.connected_action_desc, ServerData.name))
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentIntent(pendingIntent)
-                .build()
-
-        startForeground(1, notification)
+        showNotification()
 
         try {
             recorder = AudioRecord(
@@ -99,6 +83,53 @@ class AudioService : Service() {
         }
 
         startAudioThread()
+    }
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    private fun showNotification() {
+        val notificationIntent = Intent(this, MainActivity::class.java)
+
+        val pendingIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE)
+        } else {
+            PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        if (!notificationInitialized) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O)
+                createNotificationChannel()
+
+            notificationInitialized = true
+        }
+
+        val intent = Intent(this, AudioService::class.java)
+
+        val disconnectIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.getService(this, Action.DISCONNECT.code, intent, PendingIntent.FLAG_IMMUTABLE)
+        } else {
+            PendingIntent.getService(this, Action.DISCONNECT.code, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        val toggleMuteIntent = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            PendingIntent.getService(this, Action.TOGGLE_MUTE_SELF.code, intent, PendingIntent.FLAG_IMMUTABLE)
+        } else {
+            PendingIntent.getService(this, Action.TOGGLE_MUTE_SELF.code, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        val toggleMuteDrawable = if (StreamData.muted) R.drawable.ic_baseline_volume_up_24 else R.drawable.ic_baseline_volume_off_24
+        val toggleMuteString = if (StreamData.muted) getString(R.string.audio_service_notification_btn_unmute) else getString(R.string.audio_service_notification_btn_mute)
+
+        val notification: Notification =
+            NotificationCompat.Builder(this, BuildConfig.APPLICATION_ID)
+                .setContentTitle(getString(R.string.audio_service_notification_title))
+                .setContentText(getString(R.string.audio_service_notification_desc, ServerData.name))
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentIntent(pendingIntent)
+                .addAction(R.drawable.ic_baseline_phone_disabled_24, getString(R.string.audio_service_notification_btn_disconnect), disconnectIntent)
+                .addAction(toggleMuteDrawable, toggleMuteString, toggleMuteIntent)
+                .build()
+
+        startForeground(1, notification)
     }
 
     private fun startAudioThread() {
@@ -123,13 +154,21 @@ class AudioService : Service() {
     }
 
     private fun stopStream() {
+        showNotification()
+
         audioThread.interrupt()
         audioThread.join()
 
         recorder.stop()
     }
 
-    private fun toggleMute() {
+    private fun toggleMute(change: Boolean) {
+        if (change)
+        {
+            StreamData.muted = !StreamData.muted
+            OpenMic.refreshUI(this)
+        }
+
         if (StreamData.muted) {
             stopStream()
         } else {
