dc89e2ce41bb8a5457de343636bb56611ff62fa7
Marek Grzyb <grzybdev@gmail.com>
2022-08-28 22:15:28 +0200

    Re-written WifiStateReceiver

--- app/src/main/java/pl/grzybdev/openmic/client/receivers/connectors/WifiStateReceiver.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/receivers/connectors/WifiStateReceiver.kt
@@ -6,20 +6,20 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import pl.grzybdev.openmic.client.OpenMic
+import pl.grzybdev.openmic.client.enumerators.network.Connector
+import pl.grzybdev.openmic.client.enumerators.network.ConnectorState
 
 
 class WifiStateReceiver : BroadcastReceiver() {
-
-    // private val connectSignal = Signals.signal(IConnector::class)
 
     override fun onReceive(context: Context?, intent: Intent?) {
         if (intent?.action == ConnectivityManager.CONNECTIVITY_ACTION)
         {
             var isConnected = false
-
-            /*
-
-            val connectivityManager = OpenMic.App.mainActivity?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            val connectivityManager = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
 
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 val networkCapabilities = connectivityManager.activeNetwork
@@ -35,9 +35,10 @@ class WifiStateReceiver : BroadcastReceiver() {
                     }
                 }
             }
-            */
 
-            // connectSignal.dispatcher.onEvent(Connector.WiFi, if (isConnected) ConnectorStatus.CONNECTED_OR_READY else ConnectorStatus.DISABLED)
+            OpenMic.changeConnectorStatus(context, Connector.WiFi,
+                if (isConnected) ConnectorState.READY
+                else ConnectorState.NOT_READY)
         }
     }
 
