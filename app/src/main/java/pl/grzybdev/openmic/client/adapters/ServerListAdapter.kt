11be6eb0468c79788dc544f9f013067c09cccfb4
Marek Grzyb <grzybdev@gmail.com>
2022-08-29 22:38:13 +0200

    Started reworking ServerListAdapter

--- app/src/main/java/pl/grzybdev/openmic/client/adapters/ServerListAdapter.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/adapters/ServerListAdapter.kt
@@ -1,6 +1,6 @@
 package pl.grzybdev.openmic.client.adapters
 
-import android.annotation.SuppressLint
+import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,9 +9,19 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import pl.grzybdev.openmic.client.R
 import pl.grzybdev.openmic.client.dataclasses.ServerEntry
+import pl.grzybdev.openmic.client.enumerators.network.Connector
+import pl.grzybdev.openmic.client.singletons.AppData
+import pl.grzybdev.openmic.client.singletons.ServerData
 import kotlin.concurrent.thread
 
 class ServerListAdapter : RecyclerView.Adapter<ServerListAdapter.ViewHolder>() {
+
+    lateinit var ctx: Activity
+
+    fun setContext(ctx: Activity)
+    {
+        this.ctx = ctx
+    }
 
     // holder class to hold reference
     inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view), View.OnClickListener {
@@ -26,7 +36,7 @@ class ServerListAdapter : RecyclerView.Adapter<ServerListAdapter.ViewHolder>() {
 
         override fun onClick(v: View?) {
             thread(start = true) {
-                // OpenMic.App.context?.connectTo(if (serverAddress.text.split(":").size == 6) Connector.Bluetooth else Connector.WiFi, serverAddress.text.toString())
+                AppData.openmic.connectTo(ctx, if (serverAddress.text.split(":").size == 6) Connector.Bluetooth else Connector.WiFi, serverAddress.text.toString())
             }
         }
     }
@@ -38,80 +48,28 @@ class ServerListAdapter : RecyclerView.Adapter<ServerListAdapter.ViewHolder>() {
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val entries = mutableListOf<Map.Entry<String, ServerEntry>>()
 
-        /*
-        AppData.foundServers.forEach { entry -> run {
-            if (entry.value.connector == AppData.showServers)
-                entries.add(entry)
+        ServerData.foundServers.forEach { entry -> run {
+            entries.add(entry)
         }}
-         */
 
         val entryMap = entries[position]
         val entry = entryMap.value
 
-        /*
-        when (entry.serverOS)
-        {
-            ServerOS.WINDOWS -> run {
-                holder.serverOS.text = OpenMic.App.mainActivity?.getString(R.string.OS_Windows)
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                    holder.serverOS.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
-                        OpenMic.App.mainActivity?.let { AppCompatResources.getDrawable(it, R.drawable.ic_baseline_computer_24) }, null, null)
-                }
-            }
-
-            ServerOS.LINUX -> run {
-                holder.serverOS.text = OpenMic.App.mainActivity?.getString(R.string.OS_Linux)
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                    holder.serverOS.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
-                        OpenMic.App.mainActivity?.let { AppCompatResources.getDrawable(it, R.drawable.ic_baseline_computer_24) }, null, null)
-                }
-            }
-
-            ServerOS.OTHER -> run {
-                holder.serverOS.text = OpenMic.App.mainActivity?.getString(R.string.OS_Unknown)
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                    holder.serverOS.setCompoundDrawablesRelativeWithIntrinsicBounds(null,
-                        OpenMic.App.mainActivity?.let { AppCompatResources.getDrawable(it, R.drawable.ic_baseline_device_unknown_24) }, null, null)
-                }
-            }
-        }
-         */
         holder.serverName.text = entry.serverName
         holder.serverAddress.text = entry.serverIP
 
         /*
         when (entry.serverCompat) {
-            ServerCompatibility.OFFICIAL -> run { holder.serverVerifyStatus.setImageDrawable(OpenMic.App.mainActivity?.let { AppCompatResources.getDrawable(it, R.drawable.ic_baseline_verified_48) }) }
-            ServerCompatibility.UNOFFICIAL -> run { holder.serverVerifyStatus.setImageDrawable(OpenMic.App.mainActivity?.let { AppCompatResources.getDrawable(it, R.drawable.ic_baseline_not_verified_48) }) }
-            ServerCompatibility.NOT_SUPPORTED -> run { holder.serverVerifyStatus.setImageDrawable(OpenMic.App.mainActivity?.let { AppCompatResources.getDrawable(it, R.drawable.ic_baseline_block_48) }) }
-            else -> run { holder.serverVerifyStatus.setImageDrawable(OpenMic.App.mainActivity?.let { AppCompatResources.getDrawable(it, R.drawable.ic_baseline_device_unknown_24) }) }
+            ServerVersion.MATCH -> run { holder.serverVerifyStatus.setImageDrawable(AppCompatResources.getDrawable(ctx, R.drawable.ic_baseline_verified_48)) }
+            ServerVersion.UNOFFICIAL -> run { holder.serverVerifyStatus.setImageDrawable(AppCompatResources.getDrawable(ctx, R.drawable.ic_baseline_not_verified_48)) }
+            ServerVersion.MISMATCH -> run { holder.serverVerifyStatus.setImageDrawable(AppCompatResources.getDrawable(ctx, R.drawable.ic_baseline_block_48)) }
+            else -> run { holder.serverVerifyStatus.setImageDrawable(AppCompatResources.getDrawable(ctx, R.drawable.ic_baseline_device_unknown_24)) }
         }
          */
     }
 
     override fun getItemCount(): Int {
-        val servers = mutableListOf<String>()
-
-        /*
-        AppData.foundServers.forEach { entry -> run {
-            if (entry.value.connector == AppData.showServers)
-                servers.add(entry.key)
-        }}
-         */
-        return servers.size
-    }
-
-    @SuppressLint("NotifyDataSetChanged")
-    fun updateData()
-    {
-        /*
-        OpenMic.App.mainActivity?.runOnUiThread {
-            notifyDataSetChanged()
-        }
-         */
+        return ServerData.foundServers.size
     }
 
 }
