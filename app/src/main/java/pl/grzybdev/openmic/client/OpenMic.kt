c6f3333707ed5653917bb675aeded4c7acbd9119
Marek Grzyb <grzybdev@gmail.com>
2022-08-29 22:37:58 +0200

    Added Wireless related functionality in OpenMic

--- app/src/main/java/pl/grzybdev/openmic/client/OpenMic.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/OpenMic.kt
@@ -1,11 +1,15 @@
 package pl.grzybdev.openmic.client
 
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.util.Base64
 import android.util.Log
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.WebSocketListener
+import pl.grzybdev.openmic.client.dataclasses.ServerEntry
 import pl.grzybdev.openmic.client.enumerators.*
 import pl.grzybdev.openmic.client.enumerators.network.ConnectionStatus
 import pl.grzybdev.openmic.client.enumerators.network.Connector
@@ -20,9 +24,15 @@ import pl.grzybdev.openmic.client.receivers.signals.ConnectorSignalReceiver
 import pl.grzybdev.openmic.client.receivers.signals.DialogSignalReceiver
 import pl.grzybdev.openmic.client.receivers.signals.RefreshSignalReceiver
 import pl.grzybdev.openmic.client.singletons.AppData
+import pl.grzybdev.openmic.client.singletons.ServerData
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+import java.net.SocketTimeoutException
 import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.concurrent.schedule
+import kotlin.concurrent.thread
 
 
 class OpenMic : IConnection {
@@ -35,6 +45,8 @@ class OpenMic : IConnection {
         .build()
 
     private var listener: WebSocketListener? = null
+
+    private var broadcastThread: Thread? = null
 
     companion object {
         fun changeConnectionStatus(ctx: Context, status: ConnectionStatus)
@@ -158,5 +170,110 @@ class OpenMic : IConnection {
         } else if (status == ConnectionStatus.DISCONNECTED) {
             AppData.connectionListeners.remove(this)
         }
+    }
+
+    fun startWirelessScan(ctx: Activity) {
+        if (broadcastThread != null)
+            return
+
+        Log.d(javaClass.name, "startWirelessScan: Starting wireless scan...")
+
+        broadcastThread = thread(start = true) {
+            if (AppData.connectionStatus != ConnectionStatus.SELECTING_SERVER_WIFI) {
+                stopWirelessScan()
+                return@thread
+            }
+
+            val broadcastSocket = DatagramSocket(AppData.communicationPort, InetAddress.getByName("0.0.0.0"))
+            broadcastSocket.broadcast = true
+            broadcastSocket.soTimeout = 1000
+
+            val buf = ByteArray(1024)
+            val packet = DatagramPacket(buf, buf.size)
+
+            while (!Thread.interrupted()) {
+                Log.d(javaClass.name, "Waiting for broadcast on port ${AppData.communicationPort}...")
+
+                try {
+                    broadcastSocket.receive(packet)
+                } catch (e: SocketTimeoutException) {
+                    continue
+                }
+
+                handleBroadcast(ctx, packet.data, packet.address.hostAddress!!.toString())
+            }
+
+            broadcastSocket.close()
+        }
+    }
+
+    fun stopWirelessScan() {
+        Log.d(javaClass.name, "stopWirelessScan: Stopping broadcast thread...")
+
+        broadcastThread?.interrupt()
+        broadcastThread?.join()
+
+        broadcastThread = null
+    }
+
+    private fun handleBroadcast(ctx: Activity, encodedData: ByteArray, senderIP: String)
+    {
+        if (broadcastThread?.isInterrupted == true)
+            return
+
+        Log.d(javaClass.name, "Received broadcast, analyzing it...")
+        val decodedData = String(Base64.decode(encodedData, Base64.DEFAULT))
+        val data = decodedData.split(";")
+
+        if (data.size != 5)
+        {
+            Log.w(javaClass.name, "Received broadcast is invalid, ignoring...")
+            return
+        }
+
+        val entry = ServerEntry(data[3], senderIP, getServerVersion(data[0], data[1]), data[2], data[4], Connector.WiFi)
+
+        if (!ServerData.foundServers.contains(entry.serverID)) {
+            // First time we received broadcast from this server, just add it to foundServers list
+            Log.d(javaClass.name, "Received first broadcast packet from ${entry.serverID}, starting heartbeat thread...")
+
+            ServerData.foundServers[entry.serverID] = entry
+            ServerData.foundServersTimestamps[entry.serverID] = Date().time
+
+            thread(start = true) {
+                // Heartbeat thread
+                while (true) {
+                    val curTime = Date().time
+                    val lastPingTime = ServerData.foundServersTimestamps[entry.serverID] ?: break
+
+                    if (curTime - lastPingTime > 30000 || broadcastThread == null) {
+                        Log.d(javaClass.name, "${entry.serverID} did not send broadcast in more than 30 seconds, assuming it's down...")
+                        ServerData.foundServersTimestamps.remove(entry.serverID)
+                        ServerData.foundServers.remove(entry.serverID)
+
+                        updateListAdapter(ctx)
+                        break
+                    }
+
+                    updateListAdapter(ctx)
+                    Thread.sleep(1000)
+                }
+            }
+        } else {
+            // We already have this server in foundServers list, update its timestamp
+            Log.d(javaClass.name, "Received another broadcast packet from ${entry.serverID}, updating timestamp...")
+            ServerData.foundServersTimestamps[entry.serverID] = Date().time
+
+            updateListAdapter(ctx)
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateListAdapter(ctx: Activity) {
+        ctx.runOnUiThread {
+            ServerData.listAdapter.notifyDataSetChanged()
+        }
+
+        refreshUI(ctx)
     }
 }
