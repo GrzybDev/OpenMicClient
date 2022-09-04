cca60ec838dd94137aa4ede8aa7f6b1fe7329a0f
Marek Grzyb <grzybdev@gmail.com>
2022-09-04 11:33:20 +0200

    Added NO_PERMISSION ConnectorState

--- app/src/main/java/pl/grzybdev/openmic/client/enumerators/network/ConnectorState.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/enumerators/network/ConnectorState.kt
@@ -3,6 +3,7 @@ package pl.grzybdev.openmic.client.enumerators.network
 enum class ConnectorState {
     UNKNOWN,
     NOT_READY,
+    NO_PERMISSION,
     USB_CONNECTED,
     USB_CHECKING,
     USB_CONNECTED_NO_SERVER,
