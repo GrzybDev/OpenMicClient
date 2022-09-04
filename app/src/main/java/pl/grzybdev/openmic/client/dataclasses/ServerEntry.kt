c2eefc92ba0d477acc2c09e2cafd509cde347dac
Marek Grzyb <grzybdev@gmail.com>
2022-09-04 11:34:09 +0200

    Updated ServerEntry

--- app/src/main/java/pl/grzybdev/openmic/client/dataclasses/ServerEntry.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/dataclasses/ServerEntry.kt
@@ -1,12 +1,13 @@
 package pl.grzybdev.openmic.client.dataclasses
 
+import pl.grzybdev.openmic.client.enumerators.ServerOS
 import pl.grzybdev.openmic.client.enumerators.network.Connector
 import pl.grzybdev.openmic.client.enumerators.ServerVersion
 
 data class ServerEntry(val serverName: String,
                        val serverIP: String,
                        val serverCompat: ServerVersion,
-                       val serverOS: String,
+                       val serverOS: ServerOS,
                        val serverID: String,
                        val connector: Connector
 )
