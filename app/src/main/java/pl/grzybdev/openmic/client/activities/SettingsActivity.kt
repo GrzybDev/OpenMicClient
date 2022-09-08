d1118bb0f9f1630a5b1f091d337dfc04b1aa7c60
Marek Grzyb <grzybdev@gmail.com>
2022-09-08 23:48:02 +0200

    Added validator for communication port

--- app/src/main/java/pl/grzybdev/openmic/client/activities/SettingsActivity.kt
+++ app/src/main/java/pl/grzybdev/openmic/client/activities/SettingsActivity.kt
@@ -2,26 +2,46 @@ package pl.grzybdev.openmic.client.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 import pl.grzybdev.openmic.client.R
+import pl.grzybdev.openmic.client.singletons.AppData
 
 class SettingsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
+
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
                 .replace(R.id.settings, SettingsFragment())
                 .commit()
         }
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+            val port: EditTextPreference? = findPreference(getString(R.string.PREFERENCE_APP_PORT))
+            port?.setOnBindEditTextListener { editText ->
+                editText.inputType = android.text.InputType.TYPE_CLASS_NUMBER
+            }
+            
+            port?.setOnPreferenceChangeListener { _, newValue ->
+                val value = newValue.toString().toInt()
+
+                if (value in 1..65535) {
+                    AppData.communicationPort = value
+                    true
+                } else {
+                    false
+                }
+            }
         }
     }
 }
